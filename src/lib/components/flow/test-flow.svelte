efb4568a058c4046b064b3b57feaa60fbc070f1e
Mohamed Aziz Lahdheri <118969900+MedAziz218@users.noreply.github.com>
2024-10-09 16:22:25 +0200

    added horizontal-flow to test-flow

--- src/lib/components/flow/test-flow.svelte
+++ src/lib/components/flow/test-flow.svelte
@@ -1,63 +1,54 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
-	import { SvelteFlow, Controls, Background, BackgroundVariant, MiniMap, type ColorMode } from '@xyflow/svelte';
-    import {mode} from 'mode-watcher';
+	import {
+		SvelteFlow,
+		Controls,
+		Background,
+		BackgroundVariant,
+		MiniMap,
+		type ColorMode,
+		type NodeTypes,
+		type Node,
+		type Edge
+	} from '@xyflow/svelte';
+	import { mode } from 'mode-watcher';
 	// 👇 this is important! You need to import the styles for Svelte Flow to work
 	import '@xyflow/svelte/dist/style.css';
 
 	// We are using writables for the nodes and edges to sync them easily. When a user drags a node for example, Svelte Flow updates its position.
-	const nodes = writable([
-		{
-			id: '1',
-			type: 'input',
-			data: { label: 'Input Node' },
-			position: { x: 0, y: 0 }
-		},
-		{
-			id: '2',
-			type: 'default',
-			data: { label: 'Node' },
-			position: { x: 0, y: 150 }
-		}
-	]);
+	import { initialNodes, initialEdges } from './nodes-and-edges';
 
-	// same for edges
-	const edges = writable([
-		{
-			id: '1-2',
-			type: 'default',
-			source: '1',
-			target: '2',
-			label: 'Edge Text'
-		}
-	]);
+	const nodes = writable<Node[]>(initialNodes);
+
+	const edges = writable<Edge[]>(initialEdges);
 
 	const snapGrid: [number, number] = [25, 25];
-    let colorMode: ColorMode = "dark";
-    $:if ($mode === 'light') {
-        colorMode = "light";
-    } else if ($mode === 'dark') {
-        colorMode = "dark";
-    }else {
-        colorMode = "system";
-    }
+
+	let colorMode: ColorMode = 'dark';
+	$: if ($mode === 'light') {
+		colorMode = 'light';
+	} else if ($mode === 'dark') {
+		colorMode = 'dark';
+	} else {
+		colorMode = 'system';
+	}
 </script>
 
 <!--
   👇 By default, the Svelte Flow container has a height of 100%.
   This means that the parent container needs a height to render the flow.
   -->
-<div style:height="500px">
+<div style:height="100%" style:width="100%">
 	<SvelteFlow
 		{nodes}
 		{edges}
+		{colorMode}
 		{snapGrid}
-        {colorMode}
 		fitView
 		on:nodeclick={(event) => console.log('on node click', event.detail.node)}
 	>
 		<Controls />
 		<Background variant={BackgroundVariant.Dots} />
-		<MiniMap />
+		<MiniMap style="transition-duration:300ms;" class="opacity-0 hover:opacity-60 transition-opacity" />
 	</SvelteFlow>
 </div>
